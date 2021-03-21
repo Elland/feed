8a38be43bb8ab38caf709e94cbac2d45cf855c0a
Igor Ranieri <igor@elland.me>
2021-03-21 18:53:16 +0100

    Temporary: removed readme tests.

--- tests/doctest-driver.hs
+++
@@ -1,1 +0,0 @@
-{-# OPTIONS_GHC -F -pgmF doctest-driver-gen -optF README.lhs -optF -pgmL -optF markdown-unlit -optF -XOverloadedStrings -optF -XNoImplicitPrelude -optF -package -optF text -optF -package -optF xml-types #-}
